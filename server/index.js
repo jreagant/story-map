84a77a40edbfe0e27eb849c1c5b70bad3931aaa6
jreagant <jr.tankersley@gmail.com>
2024-04-02 12:49:38 -0400

    added server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
