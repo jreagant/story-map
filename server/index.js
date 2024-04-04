ecb873b5592f4122d3749b2873b7120c2622bf41
jreagant <jr.tankersley@gmail.com>
2024-04-04 16:32:41 -0400

    added routes for hero image

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
-const getLocationInfo = require('./controllers/getLocationInfo');
-const getWikiInfo = require('./controllers/getWikiInfo');
+const getNearbyLocations = require('./controllers/getNearbyLocations');
+const getWikiTitle = require('./controllers/getWikiTitle');
+const getImages = require('./controllers/getImages');
+const getHeroImage = require('./controllers/getHeroImage');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.json());
 
-app.get('/location/:name', getWikiInfo);
+app.get('/locations/nearby', getNearbyLocations);
+app.get('/location/:query', getWikiTitle);
+app.get('/images/:query', getImages);
+app.post('/images/main/', getHeroImage);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
